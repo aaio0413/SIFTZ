2a7db31e3e840a1876953c99372595b63f6f221d
AAIO1992 <takahiro04kawaguchi13@gmail.com>
2019-03-04 20:02:28 -0800

    Added original part of package.json script

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ console.log("This is __dirname!!!!!!!!", __dirname);
 app.use(express.static(path.resolve(__dirname, "../client")));
 // create home route
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+  res.sendFile(path.join(__dirname, "../client/public", "index.html"));
 });
 
 //cookie setUp
