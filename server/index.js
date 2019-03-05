ea10833292d82ee8f5f3456e0a7bb3f01885d9c5
AAIO1992 <takahiro04kawaguchi13@gmail.com>
2019-03-04 20:39:41 -0800

    So, removed yarn.lock

--- server/index.js
+++ server/index.js
@@ -95,10 +95,8 @@ else
   );
 
 app.listen(process.env.PORT || 3090, () => {
-  // const port = app.address().port;
-  console.log(
-    "app now listening for requests on port 3090 || process.env.PORT"
-  );
+  const port = app.address().port;
+  console.log("app now listening for requests on port", port);
 });
 
 process.on("SIGINT", function() {
