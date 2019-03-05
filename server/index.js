2aa831b0588ff3b11bac6437b693e66e360dc1de
AAIO1992 <takahiro04kawaguchi13@gmail.com>
2019-03-04 18:17:33 -0800

    Added error hundler and modified "/" reference again

--- server/index.js
+++ server/index.js
@@ -47,11 +47,29 @@ app.use(cors());
 // set up routes
 app.use("/api/auth", authRoutes);
 app.use("/api/mySiftz", mySiftzRoutes);
-app.use(express.static(path.resolve(__dirname, "..", "/client", "/public")));
+app.use(express.static(path.resolve(__dirname, "../client")));
 
 // create home route
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 //connect mongoDB
