e9d00980b3704ec3b0f76bb04a1edfea22d885a9
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-04-23 23:03:38 -0700

    Fix path to client/build in server.

--- server/index.js
+++ server/index.js
@@ -48,7 +48,7 @@ mongodb.connect(process.env.MONGO_DB_URL, () => {
 
 // Production:
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("..client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
   });
