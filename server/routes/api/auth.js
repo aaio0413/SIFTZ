eff0f3ea3bece88418e0abfb94c1416e4882f269
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-04-26 22:53:29 -0700

    Test routes for passport callbacks.

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -81,7 +81,7 @@ router.post('/login', (req, res) => {
 // Passport Authentication routes:ßßß
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 router.get(
-  '/auth/google/callback',
+  '/google/callback',
   passport.authenticate('google', {
     successRedirect: '/my-siftz',
     failureRedirect: '/login'
@@ -92,14 +92,14 @@ router.get(
 
 router.get('/instagram', passport.authenticate('instagram'));
 router.get(
-  '/auth/instagram/callback',
+  '/instagram/callback',
   passport.authenticate('instagram', { successRedirect: '/my-siftz',
                                        failureRedirect: '/login' }));
 
 
 router.get('/facebook', passport.authenticate('facebook'));
 router.get(
-  '/auth/facebook/callback',
+  '/facebook/callback',
   passport.authenticate('facebook', { successRedirect: '/my-siftz',
                                       failureRedirect: '/login' }));
 
